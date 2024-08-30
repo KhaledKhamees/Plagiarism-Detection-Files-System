e1acd120fe6ce3264e76c484593ebaca4cffdab7
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2024-08-30 18:05:41 +0300

    first commit

---
+++ CosineSimilarity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Plagiarism_Detection_Files_System
+{
+    internal class CosineSimilarity
+    {
+        private string Text;
+        Dictionary<string, double> VectorValues = new Dictionary<string, double>();
+        public void GetTextFromFile(FileInfo file)
+        {
+            try
+            {
+                string text = File.ReadAllText(file.FullName);
+                Text = text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+            }
+        }
+        public void VectorizeText()
+        {
+            if(string.IsNullOrEmpty(Text))
+            {
+                Console.WriteLine("Text is empty. Please load a file first.");
+                return;
+            }
+            var words = Regex.Replace(Text.ToLower(), @"\p{P}", "").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            VectorValues = words.GroupBy(word => word).ToDictionary(group => group.Key, group => (double)group.Count());
+        }
+        public double CalculateCosineSimilarity(CosineSimilarity other)
+        {
+            double dotProduct = 0.0;
+            double magnitudeA = 0.0;
+            double magnitudeB = 0.0;
+            foreach(var pair in VectorValues) 
+            {
+                double valueB = other.VectorValues.ContainsKey(pair.Key) ? other.VectorValues[pair.Key] :0.0 ;
+                dotProduct += pair.Value * valueB;
+                magnitudeA += Math.Pow(pair.Value, 2);
+            }
+            foreach(var value in other.VectorValues.Values)
+            {
+                magnitudeB += Math.Pow(value, 2);
+            }
+            magnitudeA = Math.Sqrt(magnitudeA);
+            magnitudeB = Math.Sqrt(magnitudeB);
+            return dotProduct / (magnitudeA * magnitudeB);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Plagiarism_Detection_Files_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, To Plagiarism Detection Files System!");
+            Console.WriteLine("Please Enter the Directory Path");
+            string DierctoryPath = Console.ReadLine();
+            DirectoryInfo directoryInfo= new DirectoryInfo(DierctoryPath);
+            if (directoryInfo.Exists == false )
+            {
+                Console.WriteLine("No files here , please enter the correct Path");
+                return;
+            }
+            var filesList = directoryInfo.GetFiles("*.txt"); 
+            if(filesList.Length < 2 )
+            {
+                Console.WriteLine("Not enough files to compare. Please ensure the directory contains at least two text files.");
+                return; 
+            }
+            List<CosineSimilarity> cosineSimilaritiesList = new List<CosineSimilarity>();  
+            foreach ( FileInfo file in filesList )
+            {
+                CosineSimilarity cosineSimilarity = new CosineSimilarity();
+                cosineSimilarity.GetTextFromFile(file);
+                cosineSimilarity.VectorizeText();
+                cosineSimilaritiesList.Add(cosineSimilarity);
+            }
+
+            Console.WriteLine("\nPairwise Cosine Similarity (1 indicates identical files, while 0 indicates completely different files):");
+
+            for(int i = 0;i < cosineSimilaritiesList.Count;i++)
+            {
+                for(int j =i+1; j<cosineSimilaritiesList.Count; j++)
+                {
+                    double similarity = cosineSimilaritiesList[i].CalculateCosineSimilarity(cosineSimilaritiesList[j]);
+                    Console.WriteLine($"The Cosine Similarity between {filesList[i].Name} and {filesList[j].Name} = {similarity:F4}");
+                }
+            }
+            Console.WriteLine("Processing completed.");
+            Console.ReadLine();
+        }
+    }
+}
